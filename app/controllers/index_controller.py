0af2b8f29d073f88500721131c310d63cbd0e41c
SMUS16475 <ec362@njit.edu>
2022-02-17 10:42:04 -0500

    New commit

--- app/controllers/index_controller.py
+++ app/controllers/index_controller.py
@@ -6,5 +6,5 @@ from  . controller import ControllerBase
 class IndexController(ControllerBase):
     @staticmethod
     def get():
-        name = "Random letters"
+        name = "Emmanuel Chiobi"
         return render_template('index.html', name=name)
